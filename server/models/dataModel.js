b2885663a602eba7c2b498378bf58b35a4393e27
joeljohn12 <joeljohn@ufl.edu>
2020-11-02 14:51:03 -0500

    changes to data model

--- server/models/dataModel.js
+++ server/models/dataModel.js
@@ -4,6 +4,10 @@ const formDataModel = new mongoose.Schema({
     title: { type: String, required: true},
     description: { type: String},
     typeOfDisaster: { type: String, required: true },
+    image:
+        {
+            type: Buffer, required: true
+        }
 });
 
 module.export = mongoose.model('formDataModel', formDataModel);
