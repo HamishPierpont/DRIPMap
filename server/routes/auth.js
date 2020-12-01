8a8ccef110d3581a826bfec7718a6ec4988b90ca
Hamish Pierpont <hamish.pierpont@ufl.edu>
2020-11-30 22:55:24 -0500

    Backend accepts registration requests iff the request passes validation and the email does not exist in the database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
-
+const {registerValidation} = require('./validation');
 router.post('/register', async (req, res) => {
+  
+  //Validate data first!
+  const {error} = registerValidation(req.body); 
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  
+  //Check if user already in database
+  const emailExists = await User.findOne({email: req.body.email});
+  if (emailExists) {
+    return res.status(400).send('Email already exists');
+  }
 
   const user = new User({
     name: req.body.name,
