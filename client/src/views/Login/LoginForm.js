272f73090ddaf5ea18dd710a7b3af4c4ad6eb830
KneeKhan <n.hussaini@ufl.edu>
2020-12-05 20:29:04 -0500

    Login and Register now connected to backend, and work with profile page, more map functionality added

---
+++ client/src/views/Login/LoginForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { useAlert } from 'react-alert'
+import axios from 'axios';
+import './LoginForm.css';
+import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../../shared/apiConstants';
+import { withRouter } from "react-router-dom";
+
+function LoginForm(props) {
+    const [state, setState] = useState({
+        email: "",
+        password: "",
+        successMessage: null
+    })
+    const handleChange = (e) => {
+        const { id, value } = e.target
+        setState(prevState => ({
+            ...prevState,
+            [id]: value
+        }))
+    }
+    const alert = useAlert();
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        const payload = {
+            "email": state.email,
+            "password": state.password,
+        }
+        axios.post(API_BASE_URL + '/user/login', payload)
+            .then(function (response) {
+                if (response.status === 200) {
+                    console.log(response);
+                    localStorage.setItem(ACCESS_TOKEN_NAME, response.data.token)
+                    setState(prevState => ({
+                        ...prevState,
+                        'successMessage': 'Login successful. Redirecting to home page..'
+                    }))
+                    redirectToHome();
+                    //props.showError(null)
+                }
+                else if (response.status === 204) {
+                    //props.showError("Username and password do not match");
+                    alert.show("Username and password do not match");
+                }
+                else {
+                    // props.showError("Username does not exists");
+                    alert.show("Username does not exists");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    const redirectToHome = () => {
+        props.history.push('/home');
+    }
+    const redirectToRegister = () => {
+        props.history.push('/user/register');
+    }
+    return (
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <form>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email"
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        value={state.email}
+                        onChange={handleChange}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        value={state.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="form-check">
+                </div>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onClick={handleSubmitClick}
+                >Login</button>
+            </form>
+            <div className="alert alert-success mt-2" style={{ display: state.successMessage ? 'block' : 'none' }} role="alert">
+                {state.successMessage}
+            </div>
+            <div className="registerMessage">
+                <span>Dont have an account? </span>
+                <span className="loginText" onClick={() => redirectToRegister()}>Register</span>
+            </div>
+        </div>
+    )
+}
+
+
+export default withRouter(LoginForm);
