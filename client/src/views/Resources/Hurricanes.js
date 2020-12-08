193b0d0db24587d3c82d67c4857f97a4246d5d44
KneeKhan <n.hussaini@ufl.edu>
2020-12-08 13:29:28 -0500

    Added files back in

---
+++ client/src/views/Resources/Hurricanes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './Resources.css';
+
+function Hurricanes(props) {
+    return (
+        <div className="App">
+            <header className="App-header"> Fires Page Content
+            </header>
+        </div>
+    );
+}
+
+export default withRouter(Hurricanes);
