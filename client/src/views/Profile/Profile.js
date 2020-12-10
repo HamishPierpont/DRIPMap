8b3a5da9cb4c98fdd03b4aef7d7b68309dcf9f8a
KneeKhan <n.hussaini@ufl.edu>
2020-12-09 19:31:08 -0500

    Updated profile page and removed console.logs from verify token

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -6,75 +6,171 @@ import axios from 'axios'
 
 import './Profile.css';
 
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
 function Profile(props) {
 
-  const [state, setState] = useState({
-      currentUser : "",
-      currentUserEvents : []
+
+  const libraries = ["places"];
+  const mapContainerStyle = {
+    float: 'left',
+    width: "80%",
+    height: "100vh",
+  };
+  const options = {
+    zoomControl: true
+  };
+  const center = {
+    lat: 29.6516,
+    lng: -82.3248
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCK0OcFEpHwXXXYuXYFlNwk5RBH7TaxbB8",
+    libraries,
   });
+
+  const [isLoading, setLoading] = useState(true);
+
+  const [state, setState] = useState({
+    currentUser: "",
+    currentUserEvents: []
+  });
+
+  const [selected, setSelected] = React.useState(null);
+
+  const onSelectEvent = async (event) => {
+    setSelected(event);
+  }
+
+  const onDeleteEvent = async () => {
+    console.log("Event is deleted");
+    console.log(selected);
+    return; 
+    //draft
+    // const params = {
+    //   new URLSearchParams([['_userName', localStorage.getItem('username')]]),
+    //   new URLSearchParams([['_id', selected._id]])
+    // }
+
+    // axios.deleteOne(API_BASE_URL + '/event/delete/', params, { headers: { 'auth-token': localStorage.getItem(ACCESS_TOKEN_NAME) } })
+  }
+
+  const mapRef = React.useRef();
+
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
   const alert = useAlert();
 
   useEffect(() => {
-    axios.get(API_BASE_URL + '/user/me', { headers: { 'auth-token': localStorage.getItem(ACCESS_TOKEN_NAME) } })
-      .then(function (response) {
 
-        if (response.status !== 200) {
-           props.history.push('user/login');
-        }
-        setState({currentUser: response.data.user});
-        console.log(response.data.user);
-        console.log(state.currentUser);
-      })
-      .catch(function (error) {
+    Promise.allSettled([
+      Promise.resolve(axios.get(API_BASE_URL + '/user/me', { headers: { 'auth-token': localStorage.getItem(ACCESS_TOKEN_NAME) } })),
+      Promise.resolve(axios.get(API_BASE_URL + '/event/read/', new URLSearchParams([['_userName', localStorage.getItem('username')]]), { headers: { 'auth-token': localStorage.getItem(ACCESS_TOKEN_NAME) } }))
+    ]).then(axios.spread ((userResponse, eventsResponse)=> {
+
+      if (userResponse.value.status !== 200) {
+        alert.show("Error retrieving user information. Please try logging in again.");
         props.history.push('user/login');
-      });
+      }
 
-      axios.get(API_BASE_URL + '/event/read/'+ localStorage.getItem('username'), { headers: { 'auth-token': localStorage.getItem(ACCESS_TOKEN_NAME) } })
-      .then(function (response) {
-
-        if (response.status !== 200) {
-            alert.show("Error retrieving events.");
-            console.log(response); 
-        }
-        setState({currentUserEvents: response.data.events});
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
+      if (eventsResponse.value.status !== 200) {
+        alert.show("Error retrieving events.");
+        setState({ currentUser: userResponse.data.user });
+      }else{
+        setState({ currentUserEvents: eventsResponse.value.data.events });
+        setState({ currentUser: userResponse.value.data.user });
+      }
+      setLoading(false);
+    
+    }))
+    .catch(function (error) {
+      console.log(error); 
+      props.history.push('user/login');
+    })
 
   }, []);
+   
+
 
   const handleLogout = () => {
     localStorage.removeItem(ACCESS_TOKEN_NAME);
     window.location.reload(false);
   }
 
- 
-  return  (
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  return (
     <div className="App">
       <header className="App-header">
         Profile Page Content
             </header>
-            <div>
-         {state.currentUser.name && (
-         <p> Welcome {state.currentUser.firstName} ! 
-            {state.currentUser.lastName}
-            {state.currentUser.email}
-            {state.currentUser.userName}.
-         </p>)}</div>
-         <div>
-         {state.currentUserEvents ? 
-         (<p>{state.currentUserEvents}</p>) :
-         (<p>You have not create any events. <li style={{ listStyleType: 'none', marginLeft: 250 }} class="nav-item active"><a class="nav-link" href="/events/new">Create a new event? <span class="sr-only"></span></a></li> </p>) 
-         }</div>
       <div>
-      <button style={{width: 200, height: 50, marginBottom: 50}}
-                    type="submit"
-                    className="btn btn-primary"
-                    onClick={handleLogout}
-                >Logout</button>
+        {state.currentUser.name && (
+          <p> Welcome  {state.currentUser.userName} !
+          First name: {state.currentUser.lastName} ,
+          Last name: {state.currentUser.firstName}, 
+          Email  {state.currentUser.email}
+          </p>)}</div>
+      <div>
+        {state.currentUserEvents ?
+          (state.currentUserEvents.length ?
+            (
+            <GoogleMap
+              id="map"
+              mapContainerStyle={mapContainerStyle}
+              zoom={8}
+              center={center}
+              options={options}
+              onLoad={onMapLoad}
+            >
+              {state.currentUserEvents.map((event) => (
+                <Marker
+                  key={`${event.location.lat}-${event.location.lng}`}
+                  position={event.location}
+                  onClick={() => {
+                    onSelectEvent(event);
+                  }}
+                />
+              ))}
+              {selected ?
+                (
+                  <InfoWindow
+                    position={selected.location}
+                    clickable={true}
+                    onCloseClick={() => onSelectEvent(null)}
+                  >
+                    <div>
+                      <h1>Title: {selected.title}</h1>
+                      <h1>Type: {selected.typeOfDisaster}</h1>
+                      <p>Description: {selected.description}</p>
+                      <p>Host: {selected.userName}</p>
+                      <p>Created: {selected.date}</p>
+                      <img style={{ width: 40, height: 30, backgroundColor: 'skyblue' }} src={selected.image} alt=" " />
+                      <button onClick = {onDeleteEvent}>Delete</button>
+                    </div>
+                  </InfoWindow>
+                ) : null}
+            </GoogleMap>
+      )
+          : (<p>You have not create any events. <li style={{ listStyleType: 'none', marginLeft: 250 }} class="nav-item active"><a class="nav-link" href="/events/new">Create a new event? <span class="sr-only"></span></a></li> </p>))
+          : <p>Error loading events.</p>}
+       </div>
+      <div>
+        <button style={{ width: 200, height: 50, marginBottom: 50 }}
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleLogout}
+        >Logout</button>
       </div>
     </div>
   );
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -3,13 +3,7 @@ const config = require('../config/config');
 
 //Middleware function to check validity of json web token
 module.exports = function(req, res, next) {
-    console.log("All request", req);
-    console.log("HERE HEADER", req.header);
-    console.log(req.header('auth-token'));
     const token = req.header('auth-token');
-    console.log("token", token);
-
-
     if (!token) {
         return res.status(401).send('Access Denied');
     }
