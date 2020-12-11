4a6762d9b2bc097c63f631be59d615a9a902cd5e
Hamish Pierpont <hamish.pierpont@ufl.edu>
2020-12-10 19:12:00 -0500

    Added start script for heroku

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
     }
 
     try {
-        const verified = jwt.verify(token, config.token_secret);
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET || config.token_secret);
         req.user = verified;
         next();
     }
