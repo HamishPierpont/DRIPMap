e9d050b4817dd7a3d2bb5a27fc81dd0b63b44566
Hamish Pierpont <hamish.pierpont@ufl.edu>
2020-12-10 21:05:27 -0500

    Added line

--- server/server.js
+++ server/server.js
@@ -73,6 +73,10 @@ app.post('/upload', upload.single('image'), async (req, res) => {
   }
 });
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 // Use env port or default
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
